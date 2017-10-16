0042fa8026723fb3ca6aba984df1ef70a9da5be0
Anthony-Kim <anthonykim1985@gmail.com>
2017-10-16 11:47:03 +0900

    [MODIFY] Max Concurrent

--- src/main/java/org/bigdatacenter/healthcarenhicdataextractor/config/RabbitMQConfig.java
+++ src/main/java/org/bigdatacenter/healthcarenhicdataextractor/config/RabbitMQConfig.java
@@ -52,8 +52,8 @@ public class RabbitMQConfig {
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(EXTRACTION_REQUEST_QUEUE);
         container.setMessageListener(listenerAdapter);
-//        container.setConcurrentConsumers(2);
-        container.setMaxConcurrentConsumers(1);
+        container.setConcurrentConsumers(1);
+        container.setMaxConcurrentConsumers(2);
         container.setReceiveTimeout(3000L);
         container.setRecoveryInterval(3000L);
 
